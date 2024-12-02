f92550fbf185ed0074c6fd0a75036bc4ccb62d11
stoyan <stoyan@DESKTOP-9D18QU4>
2024-12-02 08:55:00 +0200

    Add project files.

---
+++ Authentitcation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Authentitcation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
